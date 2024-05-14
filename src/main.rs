284e59351134629d082fea33db25585a3f1d3742
RedPhoenixQ <steve.dark.03@gmail.com>
2024-05-15 01:53:37 +0200

    Refactor stdin eval to function

--- src/main.rs
+++ src/main.rs
@@ -28,45 +28,50 @@ fn main() -> Result<()> {
         let ast = ast::Parser::new(tokens.into_iter(), &mut vm.strings).parse()?;
         vm.eval_iter(ast.iter())?;
     } else {
-        let mut vm = vm::VM::default();
-        let mut buf = String::new();
-        while let Ok(_result) = stdin().read_line(&mut buf) {
-            let tokens = match tokenizer::tokenize(&buf.trim()) {
-                Err(nom::Err::Incomplete(_needed)) => {
-                    println!("Incomplete, {_needed:?}");
-                    continue;
-                }
-                Err(nom::Err::Failure(err)) => {
-                    println!("Invalid syntax, {err:?}");
-                    continue;
-                }
-                Err(nom::Err::Error(err)) => {
-                    println!("Invalid syntax, {err:?}");
-                    continue;
-                }
-                Ok((rest, tokens)) => {
-                    if !rest.is_empty() {
-                        dbg!(rest);
-                    }
-                    tokens
-                }
-            };
-            dbg!(&tokens);
-            let ast = match ast::Parser::new(tokens.into_iter(), &mut vm.strings).parse() {
-                Ok(ast) => ast,
-                Err(err) => {
-                    eprintln!("Syntax error: {:?}", err);
-                    Vec::new()
-                }
-            };
-            dbg!(&ast);
+        let vm = vm::VM::default();
+        stdin_eval(vm)?;
+    }
+    Ok(())
+}
 
-            for node in &ast {
-                let _ = dbg!(vm.eval(node));
+fn stdin_eval(mut vm: vm::VM) -> Result<()> {
+    let mut buf = String::new();
+    while let Ok(_result) = stdin().read_line(&mut buf) {
+        let tokens = match tokenizer::tokenize(&buf.trim()) {
+            Err(nom::Err::Incomplete(_needed)) => {
+                println!("Incomplete, {_needed:?}");
+                continue;
             }
+            Err(nom::Err::Failure(err)) => {
+                println!("Invalid syntax, {err:?}");
+                continue;
+            }
+            Err(nom::Err::Error(err)) => {
+                println!("Invalid syntax, {err:?}");
+                continue;
+            }
+            Ok((rest, tokens)) => {
+                if !rest.is_empty() {
+                    dbg!(rest);
+                }
+                tokens
+            }
+        };
+        dbg!(&tokens);
+        let ast = match ast::Parser::new(tokens.into_iter(), &mut vm.strings).parse() {
+            Ok(ast) => ast,
+            Err(err) => {
+                eprintln!("Syntax error: {:?}", err);
+                Vec::new()
+            }
+        };
+        dbg!(&ast);
 
-            buf.clear();
+        for node in &ast {
+            let _ = dbg!(vm.eval(node));
         }
+
+        buf.clear();
     }
     Ok(())
 }
