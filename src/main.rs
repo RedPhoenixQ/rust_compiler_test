3a4bdf36ebe295c2c57eb6805950044600d6a9ad
RedPhoenixQ <steve.dark.03@gmail.com>
2024-05-19 17:09:28 +0200

    Update main stdin_eval to work with new VM

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::stdin, path::PathBuf};
+use std::{fs::read_to_string, io::stdin, path::PathBuf};
 
 use anyhow::Result;
 use clap::Parser;
@@ -14,25 +14,34 @@ fn main() -> Result<()> {
     let args = Args::parse();
     dbg!(&args);
 
-    // if let Some(file) = args.file {
-    //     let mut vm = VM::default();
-    //     vm.eval_file(&file)?;
-    //     stdin_eval(vm)?;
-    // } else {
-    //     let vm = VM::default();
-    //     stdin_eval(vm)?;
-    // }
+    if let Some(file) = args.file {
+        let code = read_to_string(file)?;
+        let ops = VM::compile_str(&code)?;
+        let mut vm = VM::default();
+        vm.eval_ops(&ops)?;
+        stdin_eval(vm)?;
+    } else {
+        let vm = VM::default();
+        stdin_eval(vm)?;
+    }
     Ok(())
 }
 
-// fn stdin_eval(mut vm: VM) -> Result<()> {
-//     let mut buf = String::new();
-//     while let Ok(_result) = stdin().read_line(&mut buf) {
-//         match vm.eval_str(&buf.trim()) {
-//             Err(err) => eprintln!("{:?}", err),
-//             Ok(value) => println!("> {value:#?}"),
-//         };
-//         buf.clear();
-//     }
-//     Ok(())
-// }
+fn stdin_eval(mut vm: VM) -> Result<()> {
+    let mut buf = String::new();
+    while let Ok(_result) = stdin().read_line(&mut buf) {
+        let ops = match VM::compile_str(&buf) {
+            Err(err) => {
+                eprintln!("{:?}", err);
+                continue;
+            }
+            Ok(ops) => ops,
+        };
+        match vm.eval_ops(&ops) {
+            Err(err) => eprintln!("{:?}", err),
+            Ok(_) => println!("> {:#?}", vm.get_accumulator()),
+        };
+        buf.clear();
+    }
+    Ok(())
+}
