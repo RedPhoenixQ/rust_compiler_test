441c14bf46378ef81c87d0500dee8c91db7493cf
RedPhoenixQ <steve.dark.03@gmail.com>
2024-05-19 16:57:34 +0200

    Replace lib VM with bytecode interpreter

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,25 @@ fn main() -> Result<()> {
     let args = Args::parse();
     dbg!(&args);
 
-    if let Some(file) = args.file {
-        let mut vm = VM::default();
-        vm.eval_file(&file)?;
-        stdin_eval(vm)?;
-    } else {
-        let vm = VM::default();
-        stdin_eval(vm)?;
-    }
+    // if let Some(file) = args.file {
+    //     let mut vm = VM::default();
+    //     vm.eval_file(&file)?;
+    //     stdin_eval(vm)?;
+    // } else {
+    //     let vm = VM::default();
+    //     stdin_eval(vm)?;
+    // }
     Ok(())
 }
 
-fn stdin_eval(mut vm: VM) -> Result<()> {
-    let mut buf = String::new();
-    while let Ok(_result) = stdin().read_line(&mut buf) {
-        match vm.eval_str(&buf.trim()) {
-            Err(err) => eprintln!("{:?}", err),
-            Ok(value) => println!("> {value:#?}"),
-        };
-        buf.clear();
-    }
-    Ok(())
-}
+// fn stdin_eval(mut vm: VM) -> Result<()> {
+//     let mut buf = String::new();
+//     while let Ok(_result) = stdin().read_line(&mut buf) {
+//         match vm.eval_str(&buf.trim()) {
+//             Err(err) => eprintln!("{:?}", err),
+//             Ok(value) => println!("> {value:#?}"),
+//         };
+//         buf.clear();
+//     }
+//     Ok(())
+// }
